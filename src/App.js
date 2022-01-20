144aad77d3ea8d6893d5cad2ee92625f90cb4cbc
Daniel <danilog1000@gmail.com>
2022-01-20 10:33:01 -0500

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,47 +1,38 @@
 import { useEffect, useState } from "react";
 import Card from "./views/Card";
+import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
   useEffect(() => {
     const getApi = async () => {
-      fetch("https://pokeapi.co/api/v2/pokemon")
+      fetch("https://pokeapi.co/api/v2/pokemon?limit=100")
       .then(resp => resp.json())
       .then(dat => {
         dat.results.forEach(value => {
             fetch(value.url)
-            .then(resp2 => resp2.json())
-            .then(dat2 => {
-              setData((prev) => [...prev, {
-                id: dat2.id,
-                name: dat2.name,
-                img: dat2.sprites.front_default,
-                type: dat2.types[0].type.name
+            .then(resp => resp.json())
+            .then(dat => {
+              setData(prev => [...prev.sort((a,b) => a.id - b.id), {
+                id: dat.id,
+                name: dat.name,
+                img: dat.sprites.front_default,
+                type: dat.types[0].type.name
               }]);
-              /* setData([...data, {
-                id: dat2.id,
-                name: dat2.name,
-                img: dat2.sprites.front_default
-              }]); */
-              /* let pokemon = {
-                id: dat2.id,
-                name: dat2.name,
-                type: dat2.types[0].type.name,
-                img: dat2.sprites.front_default
-              };
-              //let pokemons = [...pokemons, pokemon]
-              setData(data.concat(pokemon)); */
             });
         });
       })
-      console.log("DATA: "+data);
     }
     getApi();
   }, []);
 
   return (
     <div className="App">
-      {data && data.map(value => <Card key={value.id} data={value}/>)}
+      <h1 className="content__title">Pokeapi</h1>
+      <div className="content">
+        {console.log(data)}
+        {data && data.map(value => <Card key={value.id} data={value}/>)}
+      </div>
     </div>
   );
 }
--- src/views/Card/index.js
+++ src/views/Card/index.js
@@ -1,10 +1,32 @@
-export default function Card({data}){
-   return(
-      <div>
-         <img src={data.img} alt="" />
-         <p>{data.id}</p>
-         <p>{data.name}</p>
-         <p>{data.type}</p>
+import './style.css';
+
+export default function Card({ data }) {
+   const colors = {
+         fire: '#FDDFDF',
+         grass: '#DEFDE0',
+         electric: '#FCF7DE',
+         water: '#DEF3FD',
+         ground: '#f4e7da',
+         rock: '#d5d5d4',
+         fairy: '#fceaff',
+         poison: '#98d7a5',
+         bug: '#f8d5a3',
+         dragon: '#97b3e6',
+         psychic: '#eaeda1',
+         flying: '#F5F5F5',
+         fighting: '#E6E0D4',
+         normal: '#F5F5F5',
+      };
+   return (
+      <div className="card" style={{backgroundColor: colors[data.type]}}>
+         <div className="card__cont-img">
+            <img className="card__img" src={data.img} alt=""/>
+         </div>
+         <div className="card__cont-text">
+            <p className="card__p card__p--id">{data.id}</p>
+            <p className="card__p card__p--name">{data.name}</p>
+            <p className="card__p card__p--type">{data.type}</p>
+         </div>
       </div>
    );
 }
